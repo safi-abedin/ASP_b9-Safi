3467de153a1dab89548c354c10c0b8c591206c24
safi-abedin <kadersafi@2050>
2023-10-06 18:52:57 +0600

    DynamicExecution Through Reflection

---
+++ practiccesCSharp/ExternalAssemblyDynamicLib/MathOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalAssemblyDynamicLib
+{
+    public class MathOperations
+    {
+        public int Sum(int a,int b)
+        {
+            return a + b;
+        }
+
+        public int  Subtract(int a,int b)
+        {
+            if (a < b)
+            {
+                  return b- a;
+            }
+            else 
+            {
+                return a - b;
+            }  
+        }
+    }
+}
