2faeb17a9e72e89e316b30acb84d3ecf2a1e5a4b
safi-abedin <66793554+safi-abedin@users.noreply.github.com>
2023-10-19 10:30:42 +0600

    Delete Assignment1/SimpleMapperProject directory
    For Testing Purpose it was created

--- Assignment1/SimpleMapperProject/Company.cs
+++
@@ -1,5 +0,0 @@
-﻿internal class Company
-{
-    public string Name { get; set; }
-    public List<Employee> Employees { get; set; }
-}
--- Assignment1/SimpleMapperProject/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿using SimpleMapperProject;
-
-internal class Employee
-{
-    public string Name { get; set; }
-    public int EmployeeId { get; set; }
-    public Address EmployeeAddress { get; set; }
-    public List<Contact> Contacts { get; set; }
-}
