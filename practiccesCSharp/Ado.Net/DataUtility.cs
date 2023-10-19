f3fb688cb9aa16c9ab32c96bd96c8eb010b601db
safi-abedin <kadersafi@2050>
2023-10-19 13:51:00 +0600

    Practiced ado.net

---
+++ practiccesCSharp/Ado.Net/DataUtility.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.Net
+{
+    public class DataUtility
+    {
+        private readonly string _connectionString;
+
+        public DataUtility(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int WriteOperation(string commandText, Dictionary<string, object> parameterMap)
+        {
+            using SqlCommand command = GetCommand(commandText,parameterMap);
+
+            int count = command.ExecuteNonQuery();
+
+            return count;
+        }
+
+        public List<object[]> ReadOperation(string commandText,Dictionary<string, object> parameterMap)
+        {
+            using SqlCommand command = GetCommand(commandText, parameterMap);
+
+            using SqlDataReader reader = command.ExecuteReader();
+
+            List<object[]> data = new List<object[]>();
+
+            while (reader.Read())
+            {
+                object[] row = new object[reader.FieldCount];
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row[i] = reader.GetValue(i);
+                }
+
+                data.Add(row);
+            }
+
+            return data;
+        }
+
+        private SqlCommand GetCommand(string commandText, Dictionary<string, object> parameterMap)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+
+            SqlCommand command = new SqlCommand(commandText, connection);
+
+            if (command.Connection.State != System.Data.ConnectionState.Open)
+                command.Connection.Open();
+            if(parameterMap != null)
+            {
+                foreach(var p in parameterMap)
+                {
+                    command.Parameters.Add(new SqlParameter(p.Key, p.Value));
+                }
+            }
+            return command;
+        }
+
+    }
+}
---
+++ practiccesCSharp/Ado.Net/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Ado.Net;
+
+var name = "";
+var fees = 0;
+var date = default(DateTime);
+
+var ParameterMap = new Dictionary<string, object>();
+
+ParameterMap.Add("name", name);
+ParameterMap.Add("Fees", fees);
+ParameterMap.Add("Date", date);
+
+
+var connectionString = "";
+DataUtility dataUtility = new DataUtility(connectionString);
+
+var commandText = "";
+dataUtility.WriteOperation(commandText,ParameterMap);
