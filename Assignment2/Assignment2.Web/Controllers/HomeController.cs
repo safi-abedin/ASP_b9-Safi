67c7a0f72b76ac1e43f9c444f746b5a1bcbe932d
safi-abedin <kadersafi@2050>
2023-10-25 16:33:13 +0600

    Crated MVC project and tried to log in DataBase

---
+++ Assignment2/Assignment2.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Assignment2.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment2.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("I am in Index");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
