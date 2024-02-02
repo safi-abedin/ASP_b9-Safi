f31a894275e5f393782a5b92c7840e25ae69bd55
safi-abedin <kadersafi@2050>
2024-02-02 20:47:22 +0600

    Tried To handle InsertNestedObject by parent Id Reference

--- Assignment3/Assignment3/MyORM.cs
+++ Assignment3/Assignment3/MyORM.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -18,18 +19,125 @@ namespace Assignment3
 
             PropertyInfo[] properties = entityType.GetProperties();
 
+            PropertyInfo parentIdProp = entityType.GetProperty("Id");
+            var parentId = parentIdProp.GetValue(entity, null);
+
+
+            foreach(PropertyInfo property in properties)
+            {
+                if (IsNestedObject(property))
+                {
+                    InsertNestedObject(property, entity,tableName, parentId);
+                }
+                else if(property.Name == "Id") 
+                { 
+                    Console.WriteLine(property.Name + "Primptive");
+                    Console.WriteLine(parentId);
+                }
+            }
+
             // Create INSERT statement for the main entity table
             string insertMainEntity = GenerateInsertStatement(tableName, properties);
 
             // Execute the INSERT statement for the main entity
-            ExecuteNonQuery(insertMainEntity, properties, entity);
+            var id =  ExecuteNonQuery(insertMainEntity, properties, entity);
         }
 
-        private void ExecuteNonQuery(string query, PropertyInfo[] properties, object entity)
+
+        private void InsertNestedObject(PropertyInfo nestedProperty, object entity, string parentTableName, object? parentId)
+        {
+            var type = nestedProperty.PropertyType;
+            var nestedObject = nestedProperty.GetValue(entity);
+
+            if (nestedObject != null)
+            {
+                var nestedType = nestedObject.GetType();
+                var nestedTableName = nestedType.Name;
+
+                Console.WriteLine(nestedTableName);
+
+                PropertyInfo[] nestedProperties = nestedType.GetProperties();
+
+                PropertyInfo nestedIdProp = nestedType.GetProperty("Id");
+                var nestedId = nestedIdProp.GetValue(nestedObject);
+
+                foreach (var nestedProp in nestedProperties)
+                {
+                    if (IsNestedObject(nestedProp))
+                    {
+                        InsertNestedObject(nestedProp, nestedObject, nestedTableName, nestedId);
+                    }
+                    else if (nestedProp.Name == "Id")
+                    {
+                        Console.WriteLine(nestedProp.Name + "Primitive");
+                        Console.WriteLine(nestedId);
+                    }
+                }
+
+
+                var insertelements = new List<PropertyInfo>(); 
+                foreach (var nestedProp in nestedProperties)
+                {
+                    if (!IsNestedObject(nestedProp))
+                    {
+                        insertelements.Add(nestedProp);
+                    }
+                }
+
+
+
+               // Create INSERT statement for the nested entity table
+               string insertNestedEntity = GenerateNestedInsertStatement(nestedTableName, insertelements.ToArray(), parentTableName, parentId);
+
+                // Execute the INSERT statement for the nested entity
+                var nestedIdResult = ExecuteNonQuery(insertNestedEntity, insertelements.ToArray(), nestedObject);
+
+                // Use the nestedIdResult if needed
+            }
+        }
+
+        private void ExecuteNestedNonQuery(string query, PropertyInfo[] properties, object entity)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    foreach (var property in properties)
+                    {
+                        if (!IsNestedObject(property))
+                        {
+                            // Explicitly map .NET types to SQL data types
+                            SqlParameter parameter = new SqlParameter($"@{property.Name}", GetSqlDbType(property.PropertyType))
+                            {
+                                Value = property.GetValue(entity) ?? DBNull.Value
+                            };
+
+                            command.Parameters.Add(parameter);
+                        }
+                    }
+   
+                    //need to insert the parent table id through parameter
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private string GenerateNestedInsertStatement(string tableName, PropertyInfo[] properties, string parentTableName, object? parentId)
+        {
+            string columns = string.Join(", ", properties.Select(p =>  p.Name));
+            string values = string.Join(", ", properties.Select(p => $"@{p.Name}"));
+
+            return $"INSERT INTO {tableName} ({columns},{parentTableName + "Id"}) VALUES ({values},{parentId});";
+        }
+
+        private int ExecuteNonQuery(string query, PropertyInfo[] properties, object entity)
+        {
+            int id = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                        connection.Open();
 
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
@@ -47,10 +155,11 @@ namespace Assignment3
                             if (nestedObject != null)
                             {
                                 // Recursively call ExecuteNonQuery for the nested object
-                                ExecuteNonQuery(GenerateInsertStatement(property.PropertyType.Name, nestedProperties), nestedProperties, nestedObject);
+                                id = ExecuteNonQuery(GenerateInsertStatement(property.PropertyType.Name, nestedProperties), nestedProperties, nestedObject);
 
                                 // Now, get the ID of the inserted nested object
-                                int nestedObjectId = GetInsertedId(connection);
+                                int nestedObjectId = GetInsertedId(connection, property.PropertyType.Name);
+
 
                                 // Set the nested object ID in the parent entity
                                 SetPropertyValue(entity, property.Name + "Id", nestedObjectId);
@@ -76,14 +185,15 @@ namespace Assignment3
                     command.ExecuteNonQuery();
                 }
             }
+            return id;
         }
 
         private string GenerateInsertStatement(string tableName, PropertyInfo[] properties)
         {
-            string columns = string.Join(", ", properties.Select(p => p.Name));
+            string columns = string.Join(", ", properties.Select(p => IsNestedObject(p)?p.Name+"Id":p.Name));
             string values = string.Join(", ", properties.Select(p => $"@{p.Name}"));
 
-            return $"INSERT INTO {tableName} ({columns}) VALUES ({values}); SELECT SCOPE_IDENTITY();";
+            return $"INSERT INTO {tableName} ({columns}) VALUES ({values}); SELECT @@IDENTITY;";
         }
 
         private SqlDbType GetSqlDbType(Type type)
@@ -100,9 +210,11 @@ namespace Assignment3
             }
         }
 
-        private int GetInsertedId(SqlConnection connection)
+        private int GetInsertedId(SqlConnection connection, string tableName)
         {
-            using (SqlCommand command = new SqlCommand("SELECT SCOPE_IDENTITY()", connection))
+            string query = $"SELECT IDENT_CURRENT('{tableName}')";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
                 object result = command.ExecuteScalar();
 
@@ -118,6 +230,9 @@ namespace Assignment3
                 }
             }
         }
+
+        // Usage in your ExecuteNonQuery method
+
 
         private void SetPropertyValue(object obj, string propertyName, object value)
         {
