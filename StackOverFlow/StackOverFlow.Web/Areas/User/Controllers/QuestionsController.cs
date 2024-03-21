83bacb60da849e2d00a5a9c203a2f91ef55a52e2
safi-abedin <kadersafi@2050>
2024-03-21 21:53:33 +0600

    Integrated AdminLte,Created User Area

---
+++ StackOverFlow/StackOverFlow.Web/Areas/User/Controllers/QuestionsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StackOverFlow.Web.Areas.User.Controllers
+{
+    [Area("user")]
+    public class QuestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- StackOverFlow/StackOverFlow.Web/Program.cs
+++ StackOverFlow/StackOverFlow.Web/Program.cs
@@ -77,10 +77,18 @@ try
        .UseAuthorization()
        .UseSession();
 
+    app.MapControllerRoute(
+        name: "areas",
+          pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+         );
+
+    app.MapRazorPages();
 
     app.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+
     app.MapRazorPages();
 
     app.Run();
