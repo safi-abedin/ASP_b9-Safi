a0f3e0fe36500afaa9d56285fb89fafb2eb6a122
safi-abedin <kadersafi@2050>
2024-03-30 23:07:57 +0600

    Task 3 Completed

--- Exam2/Task3/Program.cs
+++ Exam2/Task3/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string[] fruits = { "apple", "banana", "mango", "goava", "strawberry", "pineapple" };
+
+
+var query = from fruit in fruits
+            group fruit by fruit.Last() into furitGroup
+            select furitGroup;
+
+foreach (var item in query)
+{
+    Console.WriteLine($"Grouped By :  {item.Key}");
+    Console.WriteLine();
+    foreach(var fruit in item)
+    {
+        Console.WriteLine($"Fruit : {fruit}");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine();
+
+}
