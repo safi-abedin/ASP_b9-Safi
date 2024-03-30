73380aa1e6d1f2f6ea1a73dffbbe203a99bc238c
safi-abedin <kadersafi@2050>
2024-03-30 22:57:15 +0600

    Task 2 completed

--- Exam2/Task2/Program.cs
+++ Exam2/Task2/Program.cs
@@ -5,3 +5,14 @@ persons.Add((1, "Jalaluddin", 42));
 persons.Add((2, "Hasan", 32));
 persons.Add((3, "Maruf", 23));
 persons.Add((4, "Hasan", 33));
+
+
+
+var query = from person in persons
+            orderby person.name, person.age
+            select person;
+
+foreach(var item in query)
+{
+    Console.WriteLine($"{item.name},{item.age}");
+}
