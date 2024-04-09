3fd15b688e4a5ce2a3df8309f4d3da9f8e4f35ae
safi-abedin <kadersafi@2050>
2024-04-10 00:27:09 +0600

    Added intitial setup for profile view

---
+++ StackOverFlow/StackOverFlow.Web/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StackOverFlow.Web.Areas.User.Controllers
+{
+    [Area("user")]
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
